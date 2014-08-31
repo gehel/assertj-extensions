10b5e599f5ccd91df1c055b4a09797ef0beb4519
gehel <guillaume.lederrey@gmail.com>
2014-08-31 18:35:27 +0200

    initial commit

---
+++ src/test/java/ch/ledcom/assertj/FsDirectoryEntryAssertTest.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2014 LedCom (guillaume.lederrey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.ledcom.assertj;
+
+import de.waldheinz.fs.FsDirectoryEntry;
+import org.junit.Test;
+import org.mockito.internal.matchers.Null;
+
+import java.io.IOException;
+import java.util.Date;
+
+import static ch.ledcom.assertj.FsDirectoryEntryAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class FsDirectoryEntryAssertTest {
+
+
+    @Test
+    public final void assertModificationDateWithPrecisionOfZero() throws IOException {
+        FsDirectoryEntry entry = mock(FsDirectoryEntry.class);
+        when(entry.getLastModified()).thenReturn(1000L);
+        when(entry.getName()).thenReturn("entry name");
+
+        assertThat(entry).hasBeenModifiedAt(new Date(1000L), 0);
+    }
+
+    @Test
+    public final void assertModificationDateWithPrecisionOfTen() throws IOException {
+        FsDirectoryEntry entry = mock(FsDirectoryEntry.class);
+        when(entry.getLastModified()).thenReturn(1000L);
+        when(entry.getName()).thenReturn("entry name");
+
+        assertThat(entry).hasBeenModifiedAt(new Date(1010L), 10);
+    }
+
+    @Test(expected = AssertionError.class)
+    public final void failForDateOutOfPrecisionRange() throws IOException {
+        FsDirectoryEntry entry = mock(FsDirectoryEntry.class);
+        when(entry.getLastModified()).thenReturn(1000L);
+        when(entry.getName()).thenReturn("entry name");
+
+        assertThat(entry).hasBeenModifiedAt(new Date(1010L), 9);
+    }
+
+    @Test(expected = AssertionError.class)
+    public final void failForNullEntry() throws IOException {
+        assertThat((FsDirectoryEntry)null).hasBeenModifiedAt(new Date(1000L), 9);
+    }
+
+}
---
+++ src/test/java/ch/ledcom/assertj/PathAssertTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2014 LedCom (guillaume.lederrey@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.ledcom.assertj;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static ch.ledcom.assertj.PathAssert.assertThat;
+
+public class PathAssertTest {
+
+    @Test
+    public void existingPath() throws IOException {
+        assertThat(Paths.get("src/test/resources/test-archive.cpio")).exists();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testingForExistenceOfNonExistingPathThrowsException() throws IOException {
+        assertThat(Paths.get("non-existing")).exists();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testingForExistenceOfNullPathThrowsException() throws IOException {
+        assertThat((Path)null).exists();
+    }
+
+    @Test
+    public void nonExistingPath() throws IOException {
+        assertThat(Paths.get("non-existing")).doesNotExist();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testingForNonExistenceOfExistingPathThrowsException() throws IOException {
+        assertThat(Paths.get("src/test/resources/test-archive.cpio")).doesNotExist();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testingForNonExistenceOfNullPathThrowsException() throws IOException {
+        assertThat((Path)null).doesNotExist();
+    }
+
+}
